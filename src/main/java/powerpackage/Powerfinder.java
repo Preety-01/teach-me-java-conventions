f19cc177ae4b3c2da0dc51878b20e1ea639a5c1d
Preety-01 <prmehra.16@gmail.com>
2022-01-11 18:42:17 +0530

    Updated Code

---
+++ src/main/java/powerpackage/Powerfinder.java
@@ -0,0 +1,8 @@
+package powerpackage;
+import java.lang.*;
+public class Powerfinder {
+    public static int powerFinder(int xyz, int pqr) {
+        int answer=(int)Math.pow(xyz, pqr);
+        return answer;
+    }
+}
